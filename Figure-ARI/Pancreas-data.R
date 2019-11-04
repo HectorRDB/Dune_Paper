b50fc2eab61f6ff4afe3bdd44378c8fb7f1c0ec6
Hector <hector.rouxdebezieux@berkeley.edu>
2019-11-04 14:59:57 -0800

    Create Pancreas-data.R

---
+++ Figure-ARI/Pancreas-data.R
@@ -0,0 +1,283 @@
+# Packages ----
+library(knitr)
+libs <- c("here", "dplyr", "ggplot2", "tidyr", "stringr", "readr", "cowplot",
+          "mclust", "RColorBrewer", "purrr", "Dune", "pracma", "devtools",
+          "pkgload")
+suppressMessages(
+  suppressWarnings(sapply(libs, require, character.only = TRUE))
+)
+rm(libs)
+
+# Helper functions ----
+setwd(here("Pancreas"))
+reload(inst("here"))
+## function to get the ARI ----
+comp_merger_with_ref <- function(clusMat, clus, ref) {
+  return(adjustedRandIndex(as.matrix(clusMat)[,clus], ref))
+}
+n_clus  <- function(clusMat, clus) {
+  return(n_distinct(as.matrix(clusMat)[,clus]))
+}
+comp_tree_with_ref <- function(x, ref) {
+  return(c("n_clus" = n_distinct(x), "ARI" = adjustedRandIndex(x, ref)))
+}
+
+## Load Dune ----
+comp_dune_ref <- function(dataset, comp = "", ref) {
+  if (comp == "") {
+    df <- readRDS(here("data", "Dune",
+                       paste0(dataset, "_mergers.rds")))
+  } else {
+    df <- readRDS(here("data", "singleTree",
+                       paste0(dataset, comp, "_merger.rds")))
+  }
+  
+  ARI_ref_sc3 <- data.frame(
+    "n_clus" = functionTracking(df, n_clus, clus = "sc3"),
+    "ARI" = functionTracking(df, comp_merger_with_ref, clus = "sc3",
+                             ref = ref))[-1, ] %>%
+    arrange(n_clus) %>%
+    distinct()
+  ARI_ref_sc3 <- trapz(x = ARI_ref_sc3$n_clus, y = ARI_ref_sc3$ARI)
+  
+  ARI_ref_seurat <- data.frame(
+    "n_clus" = functionTracking(df, n_clus, clus = "Seurat"),
+    "ARI" = functionTracking(df, comp_merger_with_ref, clus = "Seurat",
+                             ref = ref))[-1, ] %>%
+    arrange(n_clus) %>%
+    distinct()
+  ARI_ref_seurat <- trapz(x = ARI_ref_seurat$n_clus, y = ARI_ref_seurat$ARI)
+  
+  ARI_ref_monocle <- data.frame(
+    "n_clus" = functionTracking(df, n_clus, clus = "Monocle"),
+    "ARI" = functionTracking(df, comp_merger_with_ref, clus = "Monocle",
+                             ref = ref))[-1, ] %>%
+    arrange(n_clus) %>%
+    distinct()
+  ARI_ref_monocle <- trapz(x = ARI_ref_monocle$n_clus, y = ARI_ref_monocle$ARI)
+  
+  df <- data.frame("comp" = comp,
+                   "method" = c("SC3", "Monocle", "Seurat"),
+                   "AUARIC" = c(ARI_ref_sc3, ARI_ref_seurat, ARI_ref_monocle))
+  return(df)
+}
+
+## Load Tree data ----
+comp_tree_ref <- function(dataset, comp, ref, type) {
+  if (comp == "") {
+    merger <- readRDS(here("data", "Dune",
+                           paste0(dataset, "_mergers.rds")))
+  } else {
+    merger <- readRDS(here("data", "singleTree",
+                           paste0(dataset, comp, "_merger.rds")))
+  }
+  ns <- lapply(merger$currentMat, n_distinct)
+  
+  ARI_ref_sc3 <- read.csv(here("Data", "singleTree",
+                               paste0(dataset, comp, "_hierarchical_",
+                                      type, ".csv"))) %>%
+    arrange(cells) %>%
+    dplyr::select(starts_with("sc3")) %>%
+    map_df(., comp_tree_with_ref, ref = ref) %>%
+    t() %>%
+    as.data.frame() %>%
+    dplyr::rename("n_clus" = V1, "ARI" = V2) %>%
+    distinct() %>%
+    arrange(n_clus) %>%
+    filter(n_clus >= ns$sc3)
+  ARI_ref_sc3 <- trapz(x = ARI_ref_sc3$n_clus, y = ARI_ref_sc3$ARI)
+  
+  ARI_ref_seurat <- read.csv(here("Data", "singleTree",
+                                  paste0(dataset, comp, "_hierarchical_",
+                                         type, ".csv"))) %>%
+    arrange(cells) %>%
+    dplyr::select(starts_with("seurat")) %>%
+    map_df(., comp_tree_with_ref, ref = ref) %>%
+    t() %>%
+    as.data.frame() %>%
+    dplyr::rename("n_clus" = V1, "ARI" = V2) %>%
+    distinct() %>%
+    arrange(n_clus) %>%
+    filter(n_clus >= ns$Seurat)
+  ARI_ref_seurat <- trapz(x = ARI_ref_seurat$n_clus, y = ARI_ref_seurat$ARI)
+  
+  ARI_ref_monocle <- read.csv(here("Data", "singleTree",
+                                   paste0(dataset, comp, "_hierarchical_",
+                                          type, ".csv"))) %>%
+    arrange(cells) %>%
+    dplyr::select(starts_with("monocle")) %>%
+    map_df(., comp_tree_with_ref, ref = ref) %>%
+    t() %>%
+    as.data.frame() %>%
+    dplyr::rename("n_clus" = V1, "ARI" = V2) %>%
+    distinct() %>%
+    arrange(n_clus) %>%
+    filter(n_clus >= ns$Monocle)
+  ARI_ref_monocle <- trapz(x = ARI_ref_monocle$n_clus, y = ARI_ref_monocle$ARI)
+  
+  df <- data.frame("comp" = comp,
+                   "method" = c("SC3", "Monocle", "Seurat"),
+                   "AUARIC" = c(ARI_ref_sc3, ARI_ref_seurat, ARI_ref_monocle))
+  
+  return(df)
+}
+
+comp_DE_tree <- function(dataset, comp, ref) {
+  return(comp_tree_ref(dataset, comp, ref, type = "DE"))
+}
+
+comp_Dist_tree <- function(dataset, comp, ref) {
+  return(comp_tree_ref(dataset, comp, ref, type = "Dist"))
+}
+
+# Load param data (if we end up using it) ----
+load_sc3 <- function(dataset, dune) {
+  sc3_init <- dune[, "sc3.Initial"]
+  SC3 <- read.csv(here("Data", "singleMethod",
+                       paste0(dataset, "_SC3.csv"))) %>%
+    select(-X) %>%
+    arrange(cells) %>%
+    select(-cells)
+  colnames(SC3) <- str_remove(colnames(SC3), "X") %>%
+    str_replace("\\.", "-") %>% unlist
+  sc3_param <- colnames(SC3)[which.max(colSums(SC3 == sc3_init))] %>%
+    as.numeric()
+  keep <- as.numeric(colnames(SC3)) <= sc3_param
+  SC3 <- SC3[, keep]
+  colnames(SC3) <- paste("sc3", colnames(SC3), sep = "_")
+  return(SC3)
+}
+
+load_monocle <- function(dataset, dune) {
+  Monocle_init <- dune[, "Monocle.Initial"]
+  Monocle <- read.csv(here("Data", "singleMethod",
+                           paste0(dataset, "_Monocle.csv"))) %>%
+    select(-X) %>%
+    arrange(cells) %>%
+    select(-cells)
+  colnames(Monocle) <- str_remove(colnames(Monocle), "k_") %>% unlist()
+  Monocle_param <- colnames(Monocle)[which.max(colSums(Monocle == Monocle_init))] %>%
+    as.numeric()
+  keep <- as.numeric(colnames(Monocle)) >= Monocle_param
+  Monocle <- Monocle[, keep]
+  colnames(Monocle) <- paste("Monocle", colnames(Monocle), sep = "_")
+  return(Monocle)
+}
+
+load_seurat <- function(dataset, dune) {
+  Seurat_init <- dune[, "Seurat.Initial"]
+  Seurat <- read.csv(here("Data", "singleMethod",
+                          paste0(dataset, "_Seurat.csv"))) %>%
+    select(-X) %>%
+    arrange(cells) %>%
+    select(-cells)
+  colnames(Seurat) <- str_remove(colnames(Seurat), "X") %>% unlist()
+  Seurat_param <- colnames(Seurat)[which.max(colSums(Seurat == Seurat_init))]
+  k_seurat <- word(Seurat_param, 3, sep = "\\.")
+  keep <- str_detect(colnames(Seurat), k_seurat) %>% unlist()
+  Seurat <- Seurat[, keep]
+  colnames(Seurat) <- str_remove(colnames(Seurat), paste0(".", k_seurat))
+  Seurat_param <- colnames(Seurat)[which.max(colSums(Seurat == Seurat_init))] %>%
+    as.numeric()
+  keep <- as.numeric(colnames(Seurat)) <= Seurat_param
+  Seurat <- Seurat[, keep]
+  colnames(Seurat) <- paste("Seurat", colnames(Seurat), sep = "_")
+  return(Seurat)
+}
+
+comp_single_ref <- function(dataset, comp, ref) {
+  if (comp == "") {
+    merger <- readRDS(here("data", "Dune",
+                           paste0(dataset, "_mergers.rds")))
+    dune <- read.csv(here("data", "Dune",
+                          paste0(dataset, ".csv")))
+  } else {
+    dune <- read.csv(here("data", "singleTree",
+                          paste0(dataset, comp, "_Dune.csv")))
+    merger <- readRDS(here("data", "singleTree",
+                           paste0(dataset, comp, "_merger.rds")))
+  }
+  
+  ns <- lapply(merger$currentMat, n_distinct)
+  
+  dune <- dune %>% arrange(cells)
+  Seurat <- load_seurat(dataset, dune)
+  SC3 <- load_sc3(dataset, dune)
+  Monocle <- load_monocle(dataset, dune)
+  
+  ARI_ref_sc3 <- SC3 %>%
+    map_df(., comp_tree_with_ref, ref = ref) %>%
+    t() %>%
+    as.data.frame() %>%
+    dplyr::rename("n_clus" = V1, "ARI" = V2) %>%
+    distinct() %>%
+    arrange(n_clus) %>%
+    filter(n_clus >= ns$sc3)
+  ARI_ref_sc3 <- trapz(x = ARI_ref_sc3$n_clus, y = ARI_ref_sc3$ARI)
+  
+  ARI_ref_seurat <- Seurat %>%
+    map_df(., comp_tree_with_ref, ref = ref) %>%
+    t() %>%
+    as.data.frame() %>%
+    dplyr::rename("n_clus" = V1, "ARI" = V2) %>%
+    distinct() %>%
+    arrange(n_clus) %>%
+    filter(n_clus >= ns$Seurat)
+  ARI_ref_seurat <- trapz(x = ARI_ref_seurat$n_clus, y = ARI_ref_seurat$ARI)
+  
+  ARI_ref_monocle <- Monocle %>%
+    map_df(., comp_tree_with_ref, ref = ref) %>%
+    t() %>%
+    as.data.frame() %>%
+    dplyr::rename("n_clus" = V1, "ARI" = V2) %>%
+    distinct() %>%
+    arrange(n_clus) %>%
+    filter(n_clus >= ns$Monocle)
+  ARI_ref_monocle <- trapz(x = ARI_ref_monocle$n_clus, y = ARI_ref_monocle$ARI)
+  
+  df <- data.frame("comp" = comp,
+                   "method" = c("SC3", "Monocle", "Seurat"),
+                   "AUARIC" = c(ARI_ref_sc3, ARI_ref_seurat, ARI_ref_monocle))
+  
+  return(df)
+}
+
+# Create the data used for the table ----
+comp_all <- function(dataset, comp, ref){
+  df <- bind_rows(
+    "Dune" = comp_dune_ref(dataset, comp, ref),
+    "DE" = comp_DE_tree(dataset, comp, ref),
+    "Dist" = comp_Dist_tree(dataset, comp, ref),
+    "Param" = comp_single_ref(dataset, comp, ref),
+    .id = "Merge_method"
+  )
+  return(df)
+}
+
+comp_dataset <- function(dataset){
+  gold_clusters <- read.csv(here("Data", dataset, 
+                                  paste0(dataset, "_meta.csv"))) %>% 
+    arrange(X) %>%
+    `$`("cell_type1") %>%
+    as.character()
+  
+  df <- bind_rows(
+    comp_all(dataset, comp = "_comp1", ref = gold_clusters),
+    comp_all(dataset, comp = "_comp2", ref = gold_clusters),
+    comp_all(dataset, comp = "_comp3", ref = gold_clusters)
+  ) %>%
+    mutate(comp = case_when(comp == "_comp1" ~ "x1",
+                            comp == "_comp1" ~ "x2",
+                            comp == "_comp1" ~ "x3"))
+  return(df)
+}
+
+df <- bind_rows(
+    "Baron" = comp_dataset(dataset = "Baron"),
+    "Segerstolpe" = comp_dataset(dataset = "Segerstolpe"),
+    .id = "dataset"
+    )
+
+setwd("..")
+reload(inst("here"))
+write.table(df, here("Figure-ARI", "data", "Pancreas.txt"), row.names = F)
