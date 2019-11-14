4017b97a60ab7263e631dec80355357da2168705
Hector <hector.rouxdebezieux@berkeley.edu>
2019-11-13 19:05:26 -0800

    rep nearly done

--- Figure-Rep/panel-b.R
+++ Figure-Rep/panel-b.R
@@ -8,45 +8,93 @@ suppressMessages(
 )
 rm(libs)
 
-# Helper functions ----
-load_Dune <- function() {
-  df <- read.table(here("Brain", "data", "Replicability", "Dune_Smart",
-                        "Large2", "consensus_cluster_replicability.txt"))
+# Helper ----
+toRank <- function(i) {
+  case_when(
+    i == "Initial" ~ 0,
+    i == "Final" ~ 100,
+    TRUE ~ as.numeric(i)
+  )
+}
+
+interpolate <- function(df, ns) {
+  if (any(df$nb_cluster == ns)) {
+    return(df %>% filter(nb_clusters >= ns))
+  } else {
+    df <- df %>% arrange(desc(nb_clusters))
+    cutoff <- which(df$nb_clusters < ns)[1]
+    slope <- (df$fraction_replicable_cells[cutoff] - 
+                df$fraction_replicable_cells[cutoff - 1]) / 
+      (df$nb_clusters[cutoff] - df$nb_clusters[cutoff - 1])
+    intercept <- df$fraction_replicable_cells[cutoff]
+    filt <- df %>% filter(nb_clusters >= ns) %>%
+      add_row(nb_clusters = ns,
+              fraction_replicable_cells = slope * 
+                (ns - df$nb_clusters[cutoff]) + intercept)
+    return(filt %>% arrange(nb_clusters))
+  }
+}
+
+n_clus  <- function(clusMat, clus) {
+  return(n_distinct(as.matrix(clusMat)[,clus]))
+}
+
+## Load Dune ----
+comp_dune <- function() {
+  df <- read.table(here("Brain", "Data", "Replicability", "Dune_Smart",
+                        "Normal", "consensus_cluster_replicability.txt"))
   df <- df %>% filter(!str_detect(clustering_method, "Consensus")) %>%
+    mutate(nb_clusters = replicable_clusters + non_replicable_clusters) %>%
+    select(nb_clusters, fraction_replicable_cells, clustering_name)
+    
+  return(df)
+}
+## Load Tree data ----
+comp_tree <- function(comp, type) {
+  dune <- read.table(here("Brain", "Data", "Replicability", "Dune_Smart",
+                          "Normal", "consensus_cluster_replicability.txt")) %>%
+    filter(!str_detect(clustering_method, "Consensus"),
+           level == 100) %>%
     mutate(nb_clusters = replicable_clusters + non_replicable_clusters)
-  df$level <- lapply(df$level, toRank) %>% unlist()
-  df$level <- as.numeric(df$level)
+  ns <- dune$nb_clusters
+  names(ns) <- dune$clustering_name
+  
+  sep <- if_else(type == "DE", "\\.", "_")
+  df <- read.table(here("Brain", "Data", "Replicability", "SingleTree",
+                        paste0("Normal_", type),
+                        "consensus_cluster_replicability.txt")) %>%
+    mutate(level = word(clustering_method, 2, sep = sep) %>%
+             as.numeric(),
+           nb_clusters = replicable_clusters + non_replicable_clusters,
+           clustering_name = word(clustering_method, 1, sep = sep))
+  
+  Sc3 <- df %>% 
+    filter(clustering_name == "sc3") %>%
+    select(nb_clusters, fraction_replicable_cells) %>%
+    interpolate(df = ., ns = ns["sc3"])
+  
+  Seurat <- df %>% 
+    filter(clustering_name == "Seurat") %>%
+    select(nb_clusters, fraction_replicable_cells) %>%
+    interpolate(df = ., ns = ns["Seurat"])
+  
+  Monocle <- df %>% 
+    filter(clustering_name == "Monocle") %>%
+    select(nb_clusters, fraction_replicable_cells) %>%
+    interpolate(df = ., ns = ns["Monocle"])
+  
+  df <- bind_rows("sc3" = Sc3,
+                  "Monocle" = Monocle,
+                  "Seurat" = Seurat,
+                  .id = "clustering_name")
+  
   return(df)
 }
 
-load_DE_tree <- function() {
-  df <- read.table(here("Brain", "data", "Replicability", "SingleTree",
-                        "Large2_DE", "consensus_cluster_replicability.txt"))
-  df <- df %>% filter(!str_detect(clustering_method, "Consensus")) %>%
-    mutate(nb_clusters = replicable_clusters + non_replicable_clusters,
-           level = str_remove(clustering_method, "^.*\\."),
-           level = if_else(nchar(level) == 1, paste0(level, "0"), level),
-           level = as.numeric(level))
-  return(df)
+comp_DE_tree <- function(comp) {
+  return(comp_tree(comp, type = "DE"))
 }
 
-load_Dist_tree <- function() {
-  df <- read.table(here("Brain", "data", "Replicability", "SingleTree",
-                        "Large2_Dist", "consensus_cluster_replicability.txt"))
-  df <- df %>% filter(!str_detect(clustering_method, "Consensus")) %>%
-    mutate(nb_clusters = replicable_clusters + non_replicable_clusters,
-           level = str_remove(clustering_method, "^.*_"),
-           level = as.numeric(level),
-           clustering_method = str_remove(clustering_method, "_.*$"))
-  return(df)
-}
-
-load_single_method <- function() {
-  df <- bind_rows("Dune" = load_Dune(),
-                  "Hierarchical_DE" = load_DE_tree(),
-                  "Hierarchical_Dist" = load_Dist_tree(),
-                  .id = "Method") %>%
-    arrange(level) %>%
-    mutate(clustering_method = str_remove_all(clustering_method, "\\..*$"))
-  return(df)
+comp_Dist_tree <- function(comp) {
+  return(comp_tree(comp, type = "Dist"))
 }
