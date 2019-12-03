d9b807fcdd5fdd87bbf7df4e928bcac53f8225fd
Hector <hector.rouxdebezieux@berkeley.edu>
2019-12-03 15:31:36 -0800

    garbage

--- Figure-Stability/Garbage.R
+++ Figure-Stability/Garbage.R
@@ -15,23 +15,27 @@ names(comps)
 df <- map(comps, function(comp) {
   print(paste0("Comp ", comp))
   no_garb <- read.table(here("Data", "Replicability", "Dune",
-                             paste("Comp", comp), 
+                             paste0("comp", comp), 
                              "consensus_cluster_replicability.txt")) %>%
     mutate(rep = 0, nb_garb = 0)
   reps <- 1:10
-  names(reps) <- 1:10
+  names(reps) <- as.character(1:10)
   df <- map(reps, function(rep){
     sizes <- 1:3
     names(sizes) <- sizes
     sizes <- map(sizes, function(size){
       return(read.table(
         here("Data", "Replicability", "Garbage",
-             paste("Bad", comp, size, rep, sep = "-"))))
+             paste("Bad", comp, size, rep, sep = "-"),
+             "consensus_cluster_replicability.txt")))
     })
-    sizes <- bind_rows(sizes, .id = "size")
+    sizes <- bind_rows(sizes, .id = "size") %>%
+      rename("nb_garb" = size) %>%
+      mutate(nb_garb = as.numeric(nb_garb))
     return(sizes)
   })
-  df <- bind_rows(df, .id = "rep")
+  df <- bind_rows(df, .id = "rep") %>%
+    mutate(rep = as.numeric(rep))
   df <- bind_rows(df, no_garb)
 })
 
