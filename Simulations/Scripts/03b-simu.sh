c781c677a16388500365c67dd895bf16e1286257
Hector <hector.rouxdebezieux@berkeley.edu>
2020-08-25 14:42:51 -0700

    really one node

--- Simulations/Scripts/03b-simu.sh
+++ Simulations/Scripts/03b-simu.sh
@@ -2,7 +2,7 @@
 #SBATCH --mail-user=hector.rouxdebezieux@berkeley.edu
 #SBATCH --mail-type=ALL
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=8
 #SBATCH --nodes=1
 
-R CMD BATCH 02-run-clusterings.R 02-run-clusterings.out
+R CMD BATCH 03b-simu.R 03b-simu.out
