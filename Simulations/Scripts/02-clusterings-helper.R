1f02a93860ce9e5491d486526259d5c7d4afe369
Hector <hector.rouxdebezieux@berkeley.edu>
2020-09-05 07:33:51 -0700

    Update 02-clusterings-helper.R

--- Simulations/Scripts/02-clusterings-helper.R
+++ Simulations/Scripts/02-clusterings-helper.R
@@ -9,120 +9,120 @@ suppressMessages(
 rm(libs)
 
 run_clusterings <- function(sce, id) {
-  # Create data ----
-  # We follow the workflow from Duo et al 2018
-  # https://github.com/markrobinsonuzh/scRNAseq_clustering_comparison
-  
-  ## Pre-processing ----
-  clusters <- sce$Group
-  keep_features <- rowSums(counts(sce) > 0) > 0
-  sce <- sce[keep_features, ]
-  df <- perCellQCMetrics(sce)
-  df$libsize.drop <- isOutlier(df$total, nmads = 3,
-                               type = "lower", log = TRUE)
-  df$feature.drop <- isOutlier(df$detected, nmads = 3,
-                               type = "lower", log = TRUE)
-  df <- as.data.frame(df)
-  sce <- sce[, !(df$libsize.drop | df$feature.drop)]
-  clusters <- clusters[!(df$libsize.drop | df$feature.drop)]
-  sce <- computeSumFactors(sce, sizes = pmin(ncol(sce), seq(20, 120, 20)),
-                           min.mean = 0.1)
-  logcounts(sce) <- scater::normalizeCounts(sce)
-  df <- colData(sce) %>% as.data.frame() %>%
-    mutate(Batch = as.factor(Batch))
-  rownames(df) <- df$Cell
-  sSeurat <- CreateSeuratObject(counts = assays(sce)$counts, project = 'allen40K',
-                                meta.data = df)
-  sSeurat <- NormalizeData(object = sSeurat, normalization.method = "LogNormalize")
-  sSeurat <- FindVariableFeatures(object = sSeurat, mean.function = ExpMean,
-                                  dispersion.function = LogVMR, do.plot = F)
-  if (nlevels(df$Batch) > 1) {
-    sSeurat <- ScaleData(object = sSeurat, vars.to.regress = c("nCount_RNA", "Batch"))
-  } else {
-    sSeurat <- ScaleData(object = sSeurat, vars.to.regress = "nCount_RNA")
-  }
-  sSeurat <- RunPCA(object = sSeurat, ndims.print = 1, npcs = 100)
-  sSeurat <- RunUMAP(sSeurat, verbose = FALSE, dims = 1:100)
-  p <- UMAPPlot(sSeurat, group.by = "Group")
-  sce <- as.SingleCellExperiment(sSeurat)
-  sce_Seurat <- sce
-  ggsave(here("Simulations", "Figures", paste0("UMAP_", id, ".png")), p)
-  
-  # TSNE K-Means ----
-  print("... Running RtsneKmeans")
-  sce_Seurat <- scater::runPCA(sce_Seurat, ntop = 2000)
-  sce_Seurat <- scater::runTSNE(sce_Seurat, ntop = 2000, ncomponents = 3, perplexity = 30)
-  TSNE <- reducedDim(sce_Seurat, "TSNE")
-  ks <- seq(20, 50, 5)
-  names(ks) <- ks
-  K_MEANS <- map_dfc(ks, function(k){
-    return(kmeans(TSNE, centers = k)$cluster)
-  })
-  K_MEANS$cells <- rownames(TSNE)
-  write.csv(x = K_MEANS, row.names = FALSE,
-            file = here("Simulations", "Data", paste0("tSNE-KMEANS", id, ".csv")))
-  
-  # UMAP K-Means ----
-  sce_Seurat <- scater::runUMAP(sce_Seurat, ntop = 2000, ncomponents = 3)
-  UMAP <- reducedDim(sce_Seurat, "UMAP")
-  ks <- seq(20, 50, 5)
-  names(ks) <- ks
-  K_MEANS <- map_dfc(ks, function(k){
-    return(kmeans(UMAP, centers = k)$cluster)
-  })
-  K_MEANS$cells <- rownames(UMAP)
-  write.csv(x = K_MEANS, row.names = FALSE,
-            file = here("Simulations", "Data", paste0("UMAP-KMEANS", id, ".csv")))
-  
-  # RSEC ----
-  sequential <- FALSE
-  subsample <- T
-  clusterFunction <- "pam"
-  # sce <- sce_og
-  # sce <- scater::runPCA(sce)
-  # sce <- scater::runUMAP(sce)
-  reduceMeth <- reducedDimNames(sce)
-  
-  print(system.time(
-    sce <- RSEC(sce, k0s = seq(10, 50, by = 5), alphas = c(0.1, 0.3),
-                reduceMethod = reduceMeth, sequential = sequential,
-                subsample = subsample, minSizes = 1, betas = c(0.8), 
-                clusterFunction = clusterFunction, ncores = NCORES, run = TRUE,
-                isCount = FALSE, dendroReduce = reduceMeth[length(reduceMeth)],
-                dendroNDims = 50, consensusProportion = 0.7, verbose = TRUE,
-                random.seed = 23578, mergeMethod = "adjP", mergeCutoff = 0.05,
-                subsampleArgs = list(resamp.num = 50, clusterFunction = "kmeans"),
-                mergeLogFCcutoff = 1, consensusMinSize = 10
-                )
-  ))
-  
-  # Saving objects
-  saveRDS(sce, here("Simulations", "Data", paste0("Merger_", id, ".rds")))
-  
-  # Running SC3 ----
-  print("... Running SC3")
-  rowData(sce)$feature_symbol <- rownames(sce)
-  counts(sce) <- as.matrix(counts(sce))
-  logcounts(sce) <- as.matrix(logcounts(sce))
-  sce <- sc3_estimate_k(sce)
-  K <- metadata(sce)$sc3$k_estimation
-  
-  print(paste0("...... The optimal number of clusters defined by sc3 is ", K))
-  ks <- seq(from = 20, to = 50, by = 5)
-  names(ks) <- ks - K
-  
-  sc3 <- map_df(ks, function(k){
-    SC3 <- sc3(sce, ks = k, svm_max = ncol(sce) + 1, biology = FALSE, 
-               gene_filter = F, n_cores = NCORES, rand_seed = 786907)
-    SC3 <- colData(SC3)[, paste0("sc3_", k, "_clusters")] %>% as.numeric()
-    return(SC3)
-  })
-  
-  sc3$cells <- colnames(sce)
-  SC3s <- sc3
-  write.csv(x = SC3s, row.names = FALSE,
-            file = here("Simulations", "Data", paste0("SC3", id, ".csv")))
-  
+  # # Create data ----
+  # # We follow the workflow from Duo et al 2018
+  # # https://github.com/markrobinsonuzh/scRNAseq_clustering_comparison
+  # 
+  # ## Pre-processing ----
+  # clusters <- sce$Group
+  # keep_features <- rowSums(counts(sce) > 0) > 0
+  # sce <- sce[keep_features, ]
+  # df <- perCellQCMetrics(sce)
+  # df$libsize.drop <- isOutlier(df$total, nmads = 3,
+  #                              type = "lower", log = TRUE)
+  # df$feature.drop <- isOutlier(df$detected, nmads = 3,
+  #                              type = "lower", log = TRUE)
+  # df <- as.data.frame(df)
+  # sce <- sce[, !(df$libsize.drop | df$feature.drop)]
+  # clusters <- clusters[!(df$libsize.drop | df$feature.drop)]
+  # sce <- computeSumFactors(sce, sizes = pmin(ncol(sce), seq(20, 120, 20)),
+  #                          min.mean = 0.1)
+  # logcounts(sce) <- scater::normalizeCounts(sce)
+  # df <- colData(sce) %>% as.data.frame() %>%
+  #   mutate(Batch = as.factor(Batch))
+  # rownames(df) <- df$Cell
+  # sSeurat <- CreateSeuratObject(counts = assays(sce)$counts, project = 'allen40K',
+  #                               meta.data = df)
+  # sSeurat <- NormalizeData(object = sSeurat, normalization.method = "LogNormalize")
+  # sSeurat <- FindVariableFeatures(object = sSeurat, mean.function = ExpMean,
+  #                                 dispersion.function = LogVMR, do.plot = F)
+  # if (nlevels(df$Batch) > 1) {
+  #   sSeurat <- ScaleData(object = sSeurat, vars.to.regress = c("nCount_RNA", "Batch"))
+  # } else {
+  #   sSeurat <- ScaleData(object = sSeurat, vars.to.regress = "nCount_RNA")
+  # }
+  # sSeurat <- RunPCA(object = sSeurat, ndims.print = 1, npcs = 100)
+  # sSeurat <- RunUMAP(sSeurat, verbose = FALSE, dims = 1:100)
+  # p <- UMAPPlot(sSeurat, group.by = "Group")
+  # sce <- as.SingleCellExperiment(sSeurat)
+  # sce_Seurat <- sce
+  # ggsave(here("Simulations", "Figures", paste0("UMAP_", id, ".png")), p)
+  # 
+  # # TSNE K-Means ----
+  # print("... Running RtsneKmeans")
+  # sce_Seurat <- scater::runPCA(sce_Seurat, ntop = 2000)
+  # sce_Seurat <- scater::runTSNE(sce_Seurat, ntop = 2000, ncomponents = 3, perplexity = 30)
+  # TSNE <- reducedDim(sce_Seurat, "TSNE")
+  # ks <- seq(20, 50, 5)
+  # names(ks) <- ks
+  # K_MEANS <- map_dfc(ks, function(k){
+  #   return(kmeans(TSNE, centers = k)$cluster)
+  # })
+  # K_MEANS$cells <- rownames(TSNE)
+  # write.csv(x = K_MEANS, row.names = FALSE,
+  #           file = here("Simulations", "Data", paste0("tSNE-KMEANS", id, ".csv")))
+  # 
+  # # UMAP K-Means ----
+  # sce_Seurat <- scater::runUMAP(sce_Seurat, ntop = 2000, ncomponents = 3)
+  # UMAP <- reducedDim(sce_Seurat, "UMAP")
+  # ks <- seq(20, 50, 5)
+  # names(ks) <- ks
+  # K_MEANS <- map_dfc(ks, function(k){
+  #   return(kmeans(UMAP, centers = k)$cluster)
+  # })
+  # K_MEANS$cells <- rownames(UMAP)
+  # write.csv(x = K_MEANS, row.names = FALSE,
+  #           file = here("Simulations", "Data", paste0("UMAP-KMEANS", id, ".csv")))
+  # 
+  # # RSEC ----
+  # sequential <- FALSE
+  # subsample <- T
+  # clusterFunction <- "pam"
+  # # sce <- sce_og
+  # # sce <- scater::runPCA(sce)
+  # # sce <- scater::runUMAP(sce)
+  # reduceMeth <- reducedDimNames(sce)
+  # 
+  # print(system.time(
+  #   sce <- RSEC(sce, k0s = seq(10, 50, by = 5), alphas = c(0.1, 0.3),
+  #               reduceMethod = reduceMeth, sequential = sequential,
+  #               subsample = subsample, minSizes = 1, betas = c(0.8), 
+  #               clusterFunction = clusterFunction, ncores = NCORES, run = TRUE,
+  #               isCount = FALSE, dendroReduce = reduceMeth[length(reduceMeth)],
+  #               dendroNDims = 50, consensusProportion = 0.7, verbose = TRUE,
+  #               random.seed = 23578, mergeMethod = "adjP", mergeCutoff = 0.05,
+  #               subsampleArgs = list(resamp.num = 50, clusterFunction = "kmeans"),
+  #               mergeLogFCcutoff = 1, consensusMinSize = 10
+  #               )
+  # ))
+  # 
+  # # Saving objects
+  # saveRDS(sce, here("Simulations", "Data", paste0("Merger_", id, ".rds")))
+  # 
+  # # Running SC3 ----
+  # print("... Running SC3")
+  # rowData(sce)$feature_symbol <- rownames(sce)
+  # counts(sce) <- as.matrix(counts(sce))
+  # logcounts(sce) <- as.matrix(logcounts(sce))
+  # sce <- sc3_estimate_k(sce)
+  # K <- metadata(sce)$sc3$k_estimation
+  # 
+  # print(paste0("...... The optimal number of clusters defined by sc3 is ", K))
+  # ks <- seq(from = 20, to = 50, by = 5)
+  # names(ks) <- ks - K
+  # 
+  # sc3 <- map_df(ks, function(k){
+  #   SC3 <- sc3(sce, ks = k, svm_max = ncol(sce) + 1, biology = FALSE, 
+  #              gene_filter = F, n_cores = NCORES, rand_seed = 786907)
+  #   SC3 <- colData(SC3)[, paste0("sc3_", k, "_clusters")] %>% as.numeric()
+  #   return(SC3)
+  # })
+  # 
+  # sc3$cells <- colnames(sce)
+  # SC3s <- sc3
+  # write.csv(x = SC3s, row.names = FALSE,
+  #           file = here("Simulations", "Data", paste0("SC3", id, ".csv")))
+  # 
   return()
   
 }
@@ -135,7 +135,7 @@ run_merging_methods <- function(Rsec, sce, id) {
   UMAP_KMEANS <- read.csv(here("Simulations", "Data", paste0("UMAP-KMEANS", id, ".csv")),
                           stringsAsFactors = FALSE) %>%
     arrange(cells)
-  TSNE_KMEANS <- read.csv(here("Simulations", "Data", paste0("TSNE-KMEANS", id, ".csv")),
+  TSNE_KMEANS <- read.csv(here("Simulations", "Data", paste0("tSNE-KMEANS", id, ".csv")),
                           stringsAsFactors = FALSE) %>%
     arrange(cells)
 
@@ -266,7 +266,7 @@ evaluate_clustering_methods <- function(sce, id) {
   UMAP_KMEANS <- read.csv(here("Simulations", "Data", paste0("UMAP-KMEANS", id, ".csv")),
                           stringsAsFactors = FALSE) %>%
     arrange(cells)
-  TSNE_KMEANS <- read.csv(here("Simulations", "Data", paste0("TSNE-KMEANS", id, ".csv")),
+  TSNE_KMEANS <- read.csv(here("Simulations", "Data", paste0("tSNE-KMEANS", id, ".csv")),
                           stringsAsFactors = FALSE) %>%
     arrange(cells)
   
