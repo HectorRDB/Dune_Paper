5523bf3285b9e08836a4ad573a14ee06ecf928f2
Hector <hector.rouxdebezieux@berkeley.edu>
2020-10-15 10:37:14 -0700

    improvement

---
+++ Simulations/Scripts/06-simu_imp.R
@@ -0,0 +1,49 @@
+# Packages ----
+libs <- c("splatter", "here", "scater", "scran", "Seurat", "dplyr",
+          "stringr", "SingleCellExperiment", "SC3", "Rtsne", "BiocParallel",
+          "ggplot2", "purrr", "purrr", "mclust")
+suppressMessages(
+  suppressWarnings(sapply(libs, require, character.only = TRUE))
+)
+rm(libs)
+NCORES <- 32
+BiocParallel::register(MulticoreParam(NCORES))
+
+# Create data ----
+source(here("Simulations", "Scripts", "04-size-helper.R"))
+set.seed(118616)
+nCells <- c(100, 150, 200, 250, 300, 400, 500, 600, 700, 800, 900,
+            1000, 1500, 2000, 2500, 3000, 5000)
+print("Creating datasets")
+sces <- lapply(nCells, function(nCell) {
+  print(paste0(".. ", nCell))
+  return(create_and_clean(nCell))
+})
+
+print("Running clustering methods")
+clusterings <- purrr::map(sces, run_clusterings)
+
+# 3 methods, different sizes and parameters ----
+# Do the consensus
+print("Running and evaluating Dunes")
+ks <- as.character(seq(30, 50, 5))
+names(ks) <- ks
+ARIs <- purrr::map(clusterings, function(clustering) {
+  Dunes <- purrr::map(ks, function(k) {
+    df <- data.frame(cells = clustering$sc3$cells,
+                     "sc3" = clustering$sc3[, k],
+                     "UMAP_KMEANS" = clustering$UMAP_KMEANS[, k],
+                     "TSNE_KMEANS" = clustering$tSNE_KMEANS[, k]
+    )
+    return(run_Dune(df))
+  })
+  ARIs <- purrr::map2(sces, Dunes, evaluate_clustering_methods)
+  names(ARIs) <- ks
+  ARIs <- bind_rows(ARIs, .id = "param")
+  return(ARIs)
+})
+
+# Do the measures
+names(ARIs) <- nCells
+ARIs <- bind_rows(ARIs, .id = "nCells")
+write.csv(x = ARIs, file = here("Simulations", "Data", "Imp.csv"))
