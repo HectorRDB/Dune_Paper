9d2d37ed0ae2c9183d636709210c6462c3ffd6dd
Hector <hector.rouxdebezieux@berkeley.edu>
2020-10-27 22:07:15 -0700

    fractioon of common DE genes

---
+++ DE/Scripts/Helper.R
@@ -0,0 +1,88 @@
+# Real scripts ----
+Prep <- function(sce) {
+  se <- CreateSeuratObject(counts = counts(sce),
+                           min.cells = 0,
+                           min.features = 0,
+                           project = "de")
+  se <- AddMetaData(se, as.data.frame(colData(sce)))
+  se <- NormalizeData(se, verbose = FALSE)
+  se <- FindVariableFeatures(se, selection.method = 'vst', nfeatures = 10000,
+                             verbose = FALSE)
+  if ("human" %in% se@meta.data) {
+    se <- ScaleData(object = se, vars.to.regress = c("nCount_RNA", "human"),
+    verbose = FALSE)  
+  } else {
+    se <- ScaleData(object = se, vars.to.regress = "nCount_RNA",
+                     verbose = FALSE)
+  }
+  return(as.SingleCellExperiment(se))
+}
+
+de_label <- function(label, sce) {
+  colLabels(sce) <- label
+  markers <- scran::findMarkers(
+    sce, pval.type = "any", direction = "up"
+  )
+  markers <- lapply(markers, function(df){
+     df %>% 
+      as.data.frame() %>%
+      dplyr::filter(FDR < .05) %>%
+      rownames() %>%
+      data.frame(markers = .) %>%
+    return(.)
+  })
+  markers <- do.call('bind_rows', markers) %>%
+    distinct()
+  return(markers)
+}
+
+# All together----
+compute_de_comp <- function(i, sce, comps, m_locs, f) {
+  df <- read.csv(file = paste0(m_locs[i], f, "_", comps[i]))
+  print(paste0("...", names(comps[i])))
+  df <- df %>% select("cells", paste0(c("Monocle", "sc3", "Seurat"), 
+                                      rep(c(".00", ".100"), each = 3)))
+  sce <- sce[, df$cells]
+  de_genes <- lapply(df %>% select(-cells), de_label, sce = sce)
+  de_genes <- bind_rows(de_genes, .id = "label")
+  return(de_genes)
+}
+
+compute_de_sce <- function(i, sces, comps, m_locs) {
+  f <- names(sces)[i]
+  print(f)
+  sce <- Prep(sces[[i]])
+  de_genes <- lapply(1:3, compute_de_comp, sce = sce,
+                     comps = comps, m_locs = m_locs, f = f)
+  names(de_genes) <- names(comps)
+  de_genes <- bind_rows(de_genes, .id = "comp")
+  return(df)
+}
+
+all_de <- function(sce1, sce2, f, s, m_locs, comps) {
+  sces <- list(sce1, sce2)
+  names(sces) <- c(f, s)
+  de_genes <- lapply(1:2, compute_de_sce, sces = sces, 
+                    comps = comps, m_locs = m_locs)
+  names(de_genes) <- names(sces)
+  de_genes <- bind_rows(de_genes, .id = "dataset")
+  de_genes <- de_genes %>%
+    mutate(method = paste0(dataset, "_", comp, "_", label))
+  dist_mat <- matrix(0, nrow = n_distinct(de_genes$method),
+                     ncol = n_distinct(de_genes$method))
+  for (i in 1:n_distinct(de_genes$method)) {
+    for (j in 1:n_distinct(de_genes$method)) {
+      genes_i <- de_genes %>% filter(
+        method == unique(de_genes$method)[i]) 
+      genes_i <- genes_i$markers 
+      genes_j <- de_genes %>% filter(
+        method == unique(de_genes$method)[j])
+      genes_j <- genes_j$markers
+      dist_mat[i, j] <- sum(genes_i %in% genes_j) / 
+        (length(genes_i) + sum(!genes_j %in% genes_i))
+    }
+  }
+  colnames(dist_mat) <- unique(de_genes$method)
+  rownames(dist_mat) <- unique(de_genes$method)
+  return(dist_mat)
+}
---
+++ DE/Scripts/Main.R
@@ -0,0 +1,73 @@
+suppressWarnings(library(optparse))
+
+# Arguments for R Script ----
+option_list <- list(
+  make_option(c("-o", "--output"),
+              action = "store", default = NA, type = "character",
+              help = "Where to store the object after running"
+  ),
+  make_option(c("-l", "--location"),
+              action = "store", default = NA, type = "character",
+              help = "The location of the data"
+  ),
+  make_option(c("-f", "--first_name"),
+              action = "store", default = NA, type = "character",
+              help = "The name of the first dataset"
+  ),
+  make_option(c("-s", "--second_name"),
+              action = "store", default = NA, type = "character",
+          help = "The name of the second dataset"
+  ),
+  make_option(c("-m", "--merge"),
+              action = "store", default = NA, type = "character",
+              help = "The location of the merged files "
+  ),
+  make_option(c("-n", "--number_two_merge"),
+              action = "store", default = NA, type = "character",
+              help = "The location of the second merged files "
+  )
+  
+)
+
+opt <- parse_args(OptionParser(option_list = option_list))
+# Packages ----
+library(SingleCellExperiment)
+library(scater)
+library(stringr)
+library(dplyr)
+library(here)
+library(tidyr)
+library(purrr)
+library(scran)
+library(Seurat)
+source(here("DE", "Scripts", "Helper.R"))
+# Load data ----
+sce1 <- readRDS(file = paste0(opt$l, opt$f, "_filt.rds"))
+sce2 <- readRDS(file = paste0(opt$l, opt$s, "_filt.rds"))
+
+if (is.na(opt$n)) {
+  comps <- list.files(opt$m) %>%
+  str_subset("Dune_NMI.csv")  %>%
+  str_remove("_Dune_NMI.csv") %>%
+  unlist() %>%
+  word(-1, sep = "_") %>%
+  unlist() %>%
+  unique() %>%
+  paste0("_Dune_NMI.csv")
+  m_locs <- rep(opt$m, length(comps))
+} else {
+  comps <- list.files(opt$m) %>%
+  str_subset("NMI_Dune.csv")  %>%
+  str_remove("_NMI_Dune.csv") %>%
+  unlist() %>%
+  word(-1, sep = "_") %>%
+  unlist() %>%
+  unique() %>%
+  paste0("_NMI_Dune.csv")
+  comps <- c(comps, "NMI.csv")
+  m_locs <- c(rep(opt$m, length(comps) -1 ), opt$n)
+}
+
+# Make predictions
+dist_mat <- all_de(sce1, sce2, opt$f, opt$s, m_locs, comps)
+write.csv(dist_mat, file = opt$o, col.names = TRUE, row.names = TRUE)
